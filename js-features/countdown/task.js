a2b1d8da419e2a9fb2ff1c9f8f8274e8995427a0
Efimovsn <efimovsn@bk.ru>
2024-04-07 20:57:17 +0300

    первое задание выполнено

---
+++ js-features/countdown/task.js
@@ -0,0 +1,13 @@
+let currentTime = 59;
+let timer = document.getElementById('timer');
+let counter = setInterval(countDown, 1000);
+function countDown() {
+    currentTime--; 
+    timer.textContent = currentTime; 
+           
+    if (currentTime < 0) {
+        clearInterval(counter);
+        alert("Вы победили в конкурсе!");
+    }
+    
+}
